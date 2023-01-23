ec197f714cef74c8fccef53c6a6f094954b9198c
Magnus Karström <97023772+yeswebdotse@users.noreply.github.com>
2023-01-23 07:19:25 +0100

    feat (Button, FeddbackForm): Making Button-component work in FeedbackForm

--- components/FeedbackForm.jsx
+++ components/FeedbackForm.jsx
@@ -1,25 +1,47 @@
-import { useState } from 'react'
-import Card from '@/components/shared/Card'
+import { useState } from 'react';
+import Card from '@/components/shared/Card';
+import Button from '@/components/shared/Button';
 
 function FeedbackForm() {
-    const [text, setText] = useState('')
+  const [text, setText] = useState('');
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
 
-    const handleTextChange = (e) => {
-        setText(e.target.value)
+  const handleTextChange = (e) => {
+    if (text === '') {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== '' && text.trim().length <= 10) {
+      setBtnDisabled(true);
+      setMessage('Text must be at least 10 characters long');
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
     }
+
+    setText(e.target.value);
+  };
 
   return (
     <Card>
-        <form>
-            <h2>How would you rate your service with us?</h2>
-            {/* todo - Tarating select component */}
-            <div className="input-group">
-                <input onChange={handleTextChange} type="text" placeholder='Write a review' value={text}/>
-                <button type="submit">Send</button>
-            </div>
-        </form>
+      <form>
+        <h2>How would you rate your service with us?</h2>
+        {/* todo - Tarating select component */}
+        <div className="input-group">
+          <input
+            onChange={handleTextChange}
+            type="text"
+            placeholder="Write a review"
+            value={text}
+          />
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
+        </div>
+        {message && <div className="message">{message}</div>}
+      </form>
     </Card>
-  )
+  );
 }
 
-export default FeedbackForm
+export default FeedbackForm;
