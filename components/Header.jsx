ed1ac1fca01dad010472dd5cfda43ae4b29854a5
Magnus Karström <97023772+yeswebdotse@users.noreply.github.com>
2023-02-08 06:45:37 +0100

    feat (*): React/Next-CRUD-app called FeedbackUI ready!

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,17 +1,17 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
-function Header({ text, bgColor, textColor}) {
-    const headerStyles = {
-        backgroundColor: bgColor,
-        color: textColor,
-        
-        // backgroundColor: '#272932',
-        // color: '#35495E'
-    }
+function Header({ text, bgColor, textColor, fontSize }) {
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor,
+    fontSize: fontSize,
+    // backgroundColor: '#272932',
+    // color: '#35495E'
+  };
 
   return (
-    <header style={ headerStyles }>
-      <div className="container">
+    <header style={headerStyles}>
+      <div className="container flex items-center justify-center">
         <h1>{text}</h1>
       </div>
     </header>
@@ -23,12 +23,14 @@ Header.defaultProps = {
   text: 'Feedback UI',
   bgColor: 'rgba(0,0,0,0.6)',
   textColor: '#41b883',
+  fontSize: '2rem',
 };
 
 Header.propTypes = {
-    text: PropTypes.string,
-    bgColor: PropTypes.string,
-    textColor: PropTypes.string,
-}
+  text: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
+  fontSize: PropTypes.string,
+};
 
 export default Header;
